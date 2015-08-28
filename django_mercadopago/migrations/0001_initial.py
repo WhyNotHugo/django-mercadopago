369d25d86a09276d1dafe3747b921407b1f547ff
Hugo Osvaldo Barrera <hugo@barrera.io>
2015-08-28 12:34:47 -0300

    Update the initial migration
    Sorry, no backwards support yet (we *are* at 0.0!).

--- django_mercadopago/migrations/0001_initial.py
+++ django_mercadopago/migrations/0001_initial.py
@@ -11,19 +11,30 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Notification',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
+                ('topic', models.CharField(max_length=1, choices=[('o', 'Merchant Order'), ('p', 'Payment')])),
+                ('resource_id', models.CharField(max_length=46)),
+                ('processed', models.BooleanField(default=False)),
+                ('last_update', models.DateTimeField(auto_now=True)),
+            ],
+        ),
+        migrations.CreateModel(
             name='Payment',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
-                ('mp_id', models.IntegerField()),
+                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
+                ('mp_id', models.IntegerField(unique=True)),
                 ('status', models.CharField(max_length=16)),
                 ('status_detail', models.CharField(max_length=16)),
                 ('created', models.DateTimeField()),
+                ('approved', models.DateTimeField()),
             ],
         ),
         migrations.CreateModel(
             name='Preference',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
                 ('mp_id', models.CharField(max_length=46)),
                 ('payment_url', models.URLField()),
                 ('sandbox_url', models.URLField()),
@@ -34,5 +45,9 @@ class Migration(migrations.Migration):
             model_name='payment',
             name='preference',
             field=models.ForeignKey(to='mp.Preference', related_name='payments'),
+        ),
+        migrations.AlterUniqueTogether(
+            name='notification',
+            unique_together=set([('topic', 'resource_id')]),
         ),
     ]
