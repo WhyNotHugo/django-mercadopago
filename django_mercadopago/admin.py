16ef8d010b603c49ac16de3732063e3109da82e0
Hugo Osvaldo Barrera <hugo@barrera.io>
2015-09-23 23:57:39 -0300

    Add Account to the admin

--- django_mercadopago/admin.py
+++ django_mercadopago/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 
 from . import models
 
+
+@admin.register(models.Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'slug',
+    )
+    prepopulated_fields = {
+        'slug': ('name',),
+    }
+
+
 admin.site.register(models.Payment)
 admin.site.register(models.Preference)
 admin.site.register(models.Notification)
