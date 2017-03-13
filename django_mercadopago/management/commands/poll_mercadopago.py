bdec1e2102db6f6f481cbdc8f9e579c26848d5f7
Hugo Osvaldo Barrera <hugo@barrera.io>
2017-03-13 15:16:59 -0300

    Add a command to poll unpaid preferences

---
+++ django_mercadopago/management/commands/poll_mercadopago.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from django.utils.translation import ugettext as _
+
+from django_mercadopago.models import Preference
+
+
+class Command(BaseCommand):
+    help = _(
+        'Poll MercadoPago to fetch and possible updates to unpaid Preferences'
+    )
+
+    def handle(self, *args, **options):
+        for preference in Preference.objects.filter(
+                paid=False,
+            ).order_by(
+                '-pk'
+            ):
+            preference.poll_status()
