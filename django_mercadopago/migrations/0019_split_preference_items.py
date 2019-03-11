9c07a56d531ede6486ece0a55f90018cfab04deb
Hugo Osvaldo Barrera <hugo@barrera.io>
2019-03-11 18:28:59 -0300

    Add support for multiple items per preference

---
+++ django_mercadopago/migrations/0019_split_preference_items.py
@@ -0,0 +1,79 @@
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('mp', '0018_payment_mpid_biginteger'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Item',
+            fields=[
+                ('id',
+                 models.AutoField(
+                     auto_created=True,
+                     primary_key=True,
+                     serialize=False,
+                     verbose_name='ID',
+                 )),
+                (
+                    'title',
+                    models.CharField(
+                        max_length=256,
+                        verbose_name='title',
+                    ),
+                ),
+                (
+                    'currency_id',
+                    models.CharField(
+                        default='ARS',
+                        max_length=3,
+                        verbose_name='currency id',
+                    ),
+                ),
+                (
+                    'description',
+                    models.CharField(
+                        max_length=256,
+                        verbose_name='description',
+                    ),
+                ),
+                (
+                    'quantity',
+                    models.PositiveSmallIntegerField(default=1),
+                ),
+                (
+                    'unit_price',
+                    models.DecimalField(decimal_places=2, max_digits=9),
+                ),
+            ],
+            options={
+                'verbose_name': 'item',
+                'verbose_name_plural': 'items',
+            },
+        ),
+        migrations.AlterField(
+            model_name='preference',
+            name='mp_id',
+            field=models.CharField(
+                help_text=(
+                    'The id MercadoPago has assigned for this Preference'),
+                max_length=46,
+                null=True,
+                verbose_name='mercadopago id',
+            ),
+        ),
+        migrations.AddField(
+            model_name='item',
+            name='preference',
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.PROTECT,
+                related_name='items',
+                to='mp.Preference',
+                verbose_name='preference',
+            ),
+        ),
+    ]
---
+++ django_mercadopago/migrations/0020_move_preference_items.py
@@ -0,0 +1,27 @@
+from django.db import migrations
+
+
+def move_item_data(apps, schema_editor):
+    """Move Item data from Preference into the new model."""
+    Preference = apps.get_model('mp', 'Preference')
+    Item = apps.get_model('mp', 'Item')
+
+    for preference in Preference.objects.all():
+        Item.objects.create(
+            preference=preference,
+            title=preference.title,
+            description=preference.description,
+            quantity=preference.quantity,
+            unit_price=preference.price,
+        )
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('mp', '0019_split_preference_items'),
+    ]
+
+    operations = [
+        migrations.RunPython(move_item_data),
+    ]
