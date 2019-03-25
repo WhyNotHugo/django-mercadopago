e8dc2b0c43d301a58dae2adc4ec345cdeb8ac8e8
Alexis Caffa <AlexisCaffa@outlook.com>
2019-03-25 17:31:06 -0300

    tests urls must start at root according to the test_view

--- tests/urls.py
+++ tests/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import include, url
 
 
 urlpatterns = [
-    url(r'^mercadopago/', include(('django_mercadopago.urls', 'django_mercadopago'), namespace='mp')),
+    url('', include(('django_mercadopago.urls', 'django_mercadopago'), namespace='mp')),
 ]
